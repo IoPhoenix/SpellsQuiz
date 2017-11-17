83c5af2f8a5bebcd211a0990ecf1bb5e0823d118
IoPhoenix <olga.fomin26@gmail.com>
2017-11-17 15:59:01 -0800

    first commit

---
+++ js/quiz.js
@@ -0,0 +1,308 @@
+let spells = [
+     {
+        "key" : "Accio",
+        "val" : "Brings an object to you"
+    }, {
+        "key" : "Aguamenti",
+        "val" : "Creates a gush of water from the tip of the spell caster’s wand"
+    }, {
+        "key" : "Alohomora",
+        "val" : "Opens locks"
+    }, {
+        "key" : "Aparecium",
+        "val" : "Makes invisible ink become visible"
+    }, {
+        "key" : "Avada Kedavra",
+        "val" : "The Unforgivable Curse, kills your opponent"
+    }, {
+        "key" : "Avifors",
+        "val" : "Turns things into birds"
+    }, {
+        "key" : "Avis",
+        "val" : "Makes birds fly out of the end of your wand"
+    }, {
+        "key" : "Bombarda",
+        "val" : "Causes a small, locally contained explosion"
+    }, {
+        "key" : "Colloportus",
+        "val" : "Closes a door and binds it so that it can’t be opened"
+    }, {
+        "key" : "Confringo",
+        "val" : "Causes the item targeted to explode"
+    }, {
+        "key" : "Confundus",
+        "val" : "Confounds your target, or makes them temporarily confused"
+    }, {
+        "key" : "Conjunctivitis",
+        "val" : "Damages the eyesight of your opponent, making them seem to have pink eye"
+    }, {
+        "key" : "Crucio",
+        "val" : "Tortures your opponent mercilessly (The Second Unforgivable Curse)"
+    }, {
+        "key" : "Deletrius",
+        "val" : "Erases the last spell cast by a wand so that it can’t be discovered"
+    }, {
+        "key" : "Densaugeo",
+        "val" : "Makes teeth grow out of control"
+    }, {
+        "key" : "Diffindo",
+        "val" : "Makes seams split open, severs an object into two pieces"
+    }, {
+        "key" : "Dissendium",
+        "val" : "Opens a specific passageway into a cellar, may be useful in other instances; may be only a password"
+    }, {
+        "key" : "Duro",
+        "val" : "Turns an item to stone"
+    }, {
+        "key" : "Engorgio",
+        "val" : "Makes an item larger, as in swollen"
+    }, {
+        "key" : "Episkey",
+        "val" : "Heals relatively minor wounds"
+    },{
+        "key" : "Evanesco",
+        "val" : "Causes an item to immediately dissolve away, as if it had never existed"
+    }, {
+        "key" : "Expecto Patronum",
+        "val" : "Creates Patronus"
+    }, {
+        "key" : "Expelliarmus",
+        "val" : "Disarms the target of the spell, such as knocking their wand out of their hand"
+    }, {
+        "key" : "Fera Verto",
+        "val" : "Transforms animals into water goblets!"
+    }, {
+        "key" : "Ferula",
+        "val" : "Binds a broken limb with a splint and bandages, tightly wrapped"
+    }, {
+        "key" : "Fidelius",
+        "val" : "Allows a secret to be hidden within the secret keeper’s soul; very powerful spell"
+    }, {
+        "key" : "Finite Incantatem",
+        "val" : "Stops any spell"
+    },{
+        "key" : "Flagrate",
+        "val" : "Allows the user to write or draw in the air with fire"
+    }, {
+        "key" : "Flipendo",
+        "val" : "Pushes or flips something backwards"
+    }, {
+        "key" : "Furnunculus",
+        "val" : "Causes a person to break out in boils"
+    }, {
+        "key" : "Geminio",
+        "val" : "Creates a duplicate of an item (a twin, as in the zodiacal sign Gemini)"
+    }, {
+        "key" : "Homorphus",
+        "val" : "Makes a werewolf or person disguised as an animal resume their human shape"
+    }, {
+        "key" : "Immobulus",
+        "val" : "Immobilizes the target"
+    }, {
+        "key" : "Impedimenta",
+        "val" : "Puts up an impediment that slows down something or someone that is coming toward you"
+    }, {
+        "key" : "Imperio",
+        "val" : "Allows the user to assume complete control of another person (The third unforgivable curse)"
+    },{
+        "key" : "Impervius",
+        "val" : "Repels water from a surface"
+    }, {
+        "key" : "Incarcerous",
+        "val" : "Conjures up ropes, which then bind an opponent"
+    }, {
+        "key" : "Incendio",
+        "val" : "Lights a fire"
+    }, {
+        "key" : "Legilimens",
+        "val" : "Allows the user to gain access to another’s mind and memories"
+    }, {
+        "key" : "Levicorpus",
+        "val" : "Turns your opponent upside down and dangles them in thin air"
+    },{
+        "key" : "Liberacorpus",
+        "val" : "\"Liberates\", or frees a body that has been caught up by the levicorpus spell"
+    }, {
+        "key" : "Locomotor Mortis",
+        "val" : "Locks an opponent’s legs together"
+    },{
+        "key" : "Lumos",
+        "val" : "Creates light, usually by making the tip of the wand glow.  More light can be created using \"lumos maxima\""
+    }, {
+        "key" : "Mobiliarbus",
+        "val" : "Moves a tree from one place to another"
+    }, {
+        "key" : "Mobilicorpus",
+        "val" : "Moves a body from one place to another"
+    }, {
+        "key" : "Morsmordre",
+        "val" : "Summons the Dark Mark"
+    }, {
+        "key" : "Muffliato",
+        "val" : "Causes a buzzing noise to surround a limited area so that those in the area can carry on a private conversation"
+    }, {
+        "key" : "Nox",
+        "val" : "Extinguishes light, used to douse the light created by “Lumos”"
+    }, {
+        "key" : "Obliviate",
+        "val" : "Makes a person \"oblivious\", erasing their memories of an event"
+    }, {
+        "key" : "Orchideous",
+        "val" : "Conjures a bunch of flowers from the user’s wand"
+    }, {
+        "key" : "Petrificus Totalus",
+        "val" : "Petrifies an opponent totally"
+    }, {
+        "key" : "Point Me",
+        "val" : "Makes the user’s wand act like a compass"
+    }, {
+        "key" : "Portus",
+        "val" : "Turns any item into a Portkey, which can then be used to transport a person or persons to another location."
+    },{
+        "key" : "Prior Incantato",
+        "val" : "Reveals to you the last spell that a wand was used to cast"
+    }, {
+        "key" : "Protego",
+        "val" : "Protects the user, and sends a spell back on an opponent"
+    }, {
+        "key" : "Quietus",
+        "val" : "Makes things quiet, used to muffle \"Sonorus\""
+    }, {
+        "key" : "Reducio",
+        "val" : "Shrinks an item"
+    },{
+        "key" : "Reducto",
+        "val" : "Blasts solid objects into pieces"
+    }, {
+        "key" : "Relashio",
+        "val" : "Releases something from being constrained or held"
+    }, {
+        "key" : "Rennervate",
+        "val" : "Means to energize or wake up"
+    }, {
+        "key" : "Reparo",
+        "val" : "Repairs broken items"
+    }, {
+        "key" : "Repello",
+        "val" : "Repels something"
+    }, {
+        "key" : "Repello Muggletum",
+        "val" : "Makes an area invisible to Muggles"
+    }, {
+        "key" : "Revelio",
+        "val" : "Causes something that is hidden to be revealed"
+    }, {
+        "key" : "Rictusempra",
+        "val" : "Causes a person to curl up in laughter, as if being tickled"
+    },{
+        "key" : "Riddikulus",
+        "val" : "Makes a boggart assume a “ridiculous” form, thereby making it funny instead of terrifying"
+    }, {
+        "key" : "Salvio Hexia",
+        "val" : "Strengthens other protective spells, or deflects any hexes cast toward a specific location"
+    }, {
+        "key" : "Scourgify",
+        "val" : "Cleans dirt or other material off of a surface"
+    }, {
+        "key" : "Sectumsempra",
+        "val" : "Causes lacerations to appear all over an opponent’s body, as if they had been cut by an invisible sword"
+    }, {
+        "key" : "Serpensortia",
+        "val" : "Conjures a snake"
+    },{
+        "key" : "Silencio",
+        "val" : "Makes the target of the spell unable to make any sound"
+    }, {
+        "key" : "Sonorus",
+        "val" : "Amplifies the user’s voice"
+    },{
+        "key" : "Stupefy",
+        "val" : "Stupefies an opponent, or knocks them insensible temporarily"
+    }, {
+        "key" : "Tarantallegra",
+        "val" : "Forces an opponent’s legs to dance uncontrollably"
+    }, {
+        "key" : "Tergeo",
+        "val" : "Scours something clean"
+    }, {
+        "key" : "Waddiwasi",
+        "val" : "Removes a stuck object, as in a wad of gum that is stuck in a keyhole"
+    }, {
+        "key" : "Wingardium Leviosa",
+        "val" : "Allows the user to make an object levitate; the first spell taught in the Harry Potter movies"
+    }
+];
+
+// ============ VARIBALES =================
+
+let numberOfQuestions = 15,
+    numberOfQuestionsLeftField = document.querySelector('.questiions-left'),
+    numberOfCorrectAnswers = document.querySelector('.correct-num').innerHTML,
+    numberOfWrongAnswers = document.querySelector('.wrong-num').innerHTML,
+    score = document.querySelector('.score').innerHTML,
+    spellsUsed = [],
+    currentKey;
+
+const startButton = document.querySelector('.start-game'),
+    questionField = document.querySelector('.question-field h2'),
+    questionIcon = document.querySelector('.question-icon'),
+    optionsField = document.querySelector('.options-field'),
+    allKeys = _.pluck(spells, 'key');
+
+
+// ============ EVENT LISTENERS =================
+
+startButton.addEventListener('click', startGame);
+    
+
+    
+
+// ============ FUNCTIONS =================
+
+function startGame() {
+    spells = _.shuffle(spells);
+    this.style.display = 'none';
+    generateQuestion();
+    generateOptions();
+}
+
+
+function generateQuestion() {
+    const randomIndex = Math.floor(Math.random() * spells.length);
+    let randomSpell = spells[randomIndex];
+    currentKey = randomSpell.key;
+    console.log('currentKey: ' + currentKey);
+
+    // check that it hasn't been used before
+    if (spellsUsed.includes(currentKey)) generateQuestion();
+    else {
+        spellsUsed.push(currentKey);
+        randomSpell = randomSpell.val[0].toLowerCase() + randomSpell.val.slice(1);
+        questionIcon.style.visibility = 'visible';
+        let heading = document.createElement("H2"); 
+        heading.textContent = `Spell that ${randomSpell}`;
+        optionsField.appendChild(heading);
+        reduceNumberOfQuestionsLeft();
+    }
+    console.log('spellsUsed: ' + spellsUsed);
+}
+
+
+
+function generateOptions() {
+   const randomKeys = _.shuffle(allKeys).slice(0, 10);
+   randomKeys.forEach(key => {
+       let node = document.createElement("SPAN"); 
+       node.textContent = key;
+       node.className = 'option';
+       optionsField.appendChild(node);
+   });
+}
+
+
+
+function reduceNumberOfQuestionsLeft() {
+    // if (numberOfQuestionsLeft === 0) finishGame();
+    numberOfQuestions--;
+    numberOfQuestionsLeftField.innerHTML = numberOfQuestions;
+}
